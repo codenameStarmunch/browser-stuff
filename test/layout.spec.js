feb82e37c710f3d86718f483a3f06c8d1d76aaf7
codenameStarmunch <cold_storage@icloud.com>
2022-07-13 10:27:23 +0100

    Added layout spec

---
+++ test/layout.spec.js
@@ -0,0 +1,47 @@
+/** 
+* @jest-environment jsdom
+*/
+
+const fs = require("fs");
+
+const path = require("path");
+const html = fs.readFileSync(path.resolve(__dirname, "../index.html"));
+
+
+describe("index page", () => {
+
+    beforeEach(() => {
+        document.documentElement.innerHTML = html.toString();
+    })
+
+    test("It displays a relevent title", () => {
+    
+        const heading = document.getElementById("main-title");
+
+        expect(heading).toBeTruthy;
+
+
+        expect(heading.textContent).toEqual("Howdy")
+
+    })
+
+    test("It displays three divs.", () => {
+
+        const divs = document.querySelectorAll("div");
+
+        expect(divs).toBeTruthy;
+
+        expect(divs.length).toEqual(3);
+
+    })
+
+    
+
+
+
+
+
+
+
+
+})
